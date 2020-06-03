53078462839035d7b7642048b4d9b2d1dcd6fbdd
Tim Kardol <tnmkardol@hotmail.com>
2020-06-03 21:22:02 +0200

    Finish working on 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="d-flex justify-content-center align-center">
-        <p class="display-4 text-white">Error 404 | This is not the page you were looking for...</p>
+    <div class="justify-content-md-center align-items-center vh-100 ">
+        <div class="row error-margin">
+            <img src="/img/obi-wan-gif.gif" alt="obi-wan gif">
+        </div>
+        <br>
+        <br>
+        <div class="row" style="margin-left: 20%">
+            <p class="error-message">Error 404 | This is not the page you were looking for...</p>
+        </div>
+        <div class="row" style="margin-left: 42%">
+            <button class="btn btn-outline-light"><a class="hover-not-found" href="/">Click here to return to the homepage</a></button>
+        </div>
     </div>
 @endsection
