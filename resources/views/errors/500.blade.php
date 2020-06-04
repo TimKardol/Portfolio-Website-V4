2222325f82f58978560dda619187a7ce75259216
Tim Kardol <tnmkardol@hotmail.com>
2020-06-04 10:45:26 +0200

    Finish 500 error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,13 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Internal Server Error</title>
-</head>
-<body>
-    <h1>Probeer de pagina te verversen.</h1>
-</body>
-</html>
+@extends('layouts.layout')
+@section('content')
+    <div class="justify-content-md-center align-items-center vh-100 ">
+        <div class="row" style="margin-left: 25%; margin-top: 250px">
+            <p class="error-message">Error 500 | Something went wrong on our side</p>
+        </div>
+        <div class="row" style="margin-left: 21.5%">
+            <p class="error-message">Try refreshing or come back later if that doesn't work</p>
+        </div>
+        <div class="row" style="margin-left: 42%">
+            <button class="btn btn-outline-light"><a class="hover-not-found" href="/">Click here to return to the homepage</a></button>
+        </div>
+    </div>
+@endsection
