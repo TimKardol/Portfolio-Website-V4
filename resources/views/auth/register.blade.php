a1fdb45f1db96ba13d639c30305a0664689f4048
Tim <tnmkardol@hotmail.com>
2020-06-07 14:55:03 +0200

    Change localisation on register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                             @csrf
 
                             <div class="form-group row">
-                                <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('Voornaam*') }}</label>
+                                <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First Name*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
@@ -26,7 +26,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Achternaam*') }}</label>
+                                <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last Name*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
@@ -40,7 +40,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('E-Mail Adres*') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('E-Mail Address*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -54,7 +54,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Wachtwoord*') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Password*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -70,7 +70,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Wachtwoord Bevestigen*') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Confirm Password*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -80,7 +80,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="register-button">
-                                        {{ __('Registreer') }}
+                                        {{ __('Register') }}
                                     </button>
                                 </div>
                             </div>
