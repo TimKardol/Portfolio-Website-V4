80e900d57b656838082fa5b1805005c9e3e00e2d
Tim <tnmkardol@hotmail.com>
2020-06-09 13:53:38 +0200

    Fix the form validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="register-card">
-                    <div class="register-card-header">{{ __('Registreren') }}</div>
+                    <div class="register-card-header">{{ __('Register') }}</div>
 
                     <div class="register-card-body">
                         <form method="POST" action="{{ route('register') }}">
@@ -40,21 +40,21 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('E-Mail Address*') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"required autocomplete="email">
 
                                     @error('email')
-                                    <span class="" role="">
-                                        <p class="alert alert-danger">{{ $message }}</p>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
                                     </span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Password*') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -70,7 +70,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right temporary-name">{{ __('Confirm Password*') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password*') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
