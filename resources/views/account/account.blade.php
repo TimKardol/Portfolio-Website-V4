4ad57db2f380461305a04a74404d78fec91dd658
Tim <tnmkardol@hotmail.com>
2020-06-12 23:41:45 +0200

    Add adminlte partials to account page

--- resources/views/account/account.blade.php
+++ resources/views/account/account.blade.php
@@ -1,23 +1,15 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <p>Welkom {{ Auth::user()->first_name }}!</p>
+@stop
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
