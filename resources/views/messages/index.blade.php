69876ffe14eb943c4de5130d029a662a6fa322e2
Tim <tnmkardol@hotmail.com>
2020-06-14 00:56:13 +0200

    Add button for creating a new message

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.layout')
 
 @section('content')
-
+    <div class="justify-content-center vh-100">
+        <div class="d-flex justify-content-center">
+            <p class="top-message">Leave a nice message here!</p>
+        </div>
+        <div class="d-flex justify-content-center">
+            <button class="btn btn-outline-light"><a class="hover-message" type="submit" href="/messages/create">Create a new message</a></button>
+        </div>
+    </div>
 @endsection
