334b465e2083de8228da158075e1fd4d261015fe
Tim <tnmkardol@hotmail.com>
2020-06-14 01:48:34 +0200

    Add fillable to model

--- app/Message.php
+++ app/Message.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    protected $fillable = ["user_id", "title", "body"]; //← only the field names inside the array can be mass-assigned
+
+
     public function user()
     {
         return $this->belongsTo(User::class);
